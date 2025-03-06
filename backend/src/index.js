78996eed60e66ff634d5880ef3b7c4ddb03b0298
ramajaiswal08 <ramajaiswal829111@gmail.com>
2025-03-06 23:54:09 +0530

    project completed

---
+++ backend/src/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 10000;
+const __dirname = path.resolve();
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://full-stack-chatapp-1aq1.onrender.com", // Your deployed frontend
+];
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+  console.log(`🚀 Server running on PORT: ${PORT}`);
+  connectDB();
+});
